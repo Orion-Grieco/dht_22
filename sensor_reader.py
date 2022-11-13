a5a12d0438f712e175bb9b1cdac1eaaffed414f4
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2022-11-12 20:51:02 -0500

    Update sensor_reader.py

--- sensor_reader.py
+++ sensor_reader.py
@@ -1,30 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import pandas as pd
-#import os
-
-
-# In[28]:
-
+import numpy as np
+import os
 
 file = pd.read_csv("natureCenterDataExample.csv")
 frame = pd.DataFrame(file, columns =["Date","Time","Entrance Humidity","Entrance C Temp","Entrance F Temp","Entrance C Heat Index","Entrance F Heat Index","Pond Humidity","Pond C Temp","Pond F Temp","Pond C Heat Index","Pond F Heat Index","Stadium Humidity","Stadium C Temp","Stadium F Temp","Stadium C Heat Index","Stadium F Heat Index"])
 frame.fillna(0) # Replaces all null values represented by NaN to 0, and inherently formats the data of the data-frame into a data-table and then subsequently prints the data-table. Works in this manner only when the method is called/used by itself, otherwise it only replaces NaN values
 
 
-# In[26]:
-
-
 frameNew = frame.fillna(0)
 print(frameNew)
 frame.fillna(0)
-
-
-# In[29]:
 
 
 import pandas as pd
@@ -37,7 +22,6 @@ print(entranceTempFrame["Entrance C Heat Index"].sum()/(39-19),": Mean of Entran
 print(entranceTempFrame["Entrance F Heat Index"].sum()/(39-19),": Mean of Entrance F Heat Index Values")
 
 print("\n")
-import numpy as np
 
 odd = pd.DataFrame(file,columns=[])
 odds = odd[odd.index%2==1]
@@ -59,10 +43,5 @@ print(entranceTempFrame["Entrance C Temp"].sum()/(oddsum1),": Mean of Entrance C
 print(entranceTempFrame["Entrance F Temp"].sum()/(oddsum1),": Mean of Entrance F Temperature Values")
 print(entranceTempFrame["Entrance C Heat Index"].sum()/(oddsum1),": Mean of Entrance C Heat Index Values")
 print(entranceTempFrame["Entrance F Heat Index"].sum()/(oddsum1),": Mean of Entrance F Heat Index Values")
-
-
-# In[ ]:
-
-
 
 
